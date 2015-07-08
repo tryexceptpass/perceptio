43045d1ffb5247e9e34bd7a7e6e18e4a8200c39d
tryexceptpass <@tryexceptpass>
2015-07-07 20:12:37 -0400

    Some experiments with skimage

--- visualize.py
+++ visualize.py
@@ -1,12 +1,13 @@
 """ File Visualizer.
 
 Usage:
-    visualize.py <filename> [--mmap | --np-memmap | --np-fullcolor]
+    visualize.py <filename> [--mmap | --np-mmap | --np-memmap | --np-fullcolor]
 
 Options:
     --np-fullcolor  Use numpy to extract data in 4-byte chunks and draw in full color
     --np-memmap     Use numpy but run it with .memmap instead of .fromfile
     --mmap          Use mmap and regular file i/o instead of numpy fromfile.
+    --np-mmap       Use mmap but stick it in a numpy array and use skimage
     --version       Show version.
     -h --help       Show this screen.
 """
@@ -17,9 +18,10 @@ import math
 import numpy as np
 import struct
 from docopt import docopt
+from skimage import io
 
 def npfullcolor(path):
-    """Show a square image that represents the speciied file using numpy.fromfile"""
+    """Show a square image that represents the speciied file using numpy.memmap"""
 
     data = np.memmap(path, dtype=np.uint32, mode='r')
 
@@ -37,7 +39,7 @@ def npfullcolor(path):
     img.show()
 
 def npmemmaprepresent(path):
-    """Show a square image that represents the speciied file using numpy.fromfile"""
+    """Show a square image that represents the speciied file using numpy.memmap"""
 
     data = np.memmap(path, dtype=np.uint8, mode='r')
 
@@ -73,6 +75,28 @@ def nprepresent(path):
     img.show()
 
 
+def skrepresent(path):
+    """Show a square image that represents the file specified starting from an np.zeros array"""
+
+    data = None
+    with open(path, 'rb') as infile:
+        data = bytearray(mmap.mmap(infile.fileno(), 0, access=mmap.ACCESS_READ))
+    side = math.ceil(math.sqrt(len(data)))
+
+    #img = Image.new('RGB', (side, side), "black")
+    pixels = np.zeros(shape=(side, side, 3), dtype=np.uint8)
+
+    for i in range(side):
+        for j in range(side):
+            index = i*side+j
+            if index < len(data):
+                pixels[i, j, 0] = data[index]
+                pixels[i, j, 1] = data[index]
+                pixels[i, j, 2] = 0
+    
+    io.imshow(pixels)
+    io.show()
+
 def represent(path):
     """Show a square image that represents the file specified"""
 
@@ -94,6 +118,8 @@ if __name__ == '__main__':
 
     if ARGS['--mmap']:
         represent(ARGS['<filename>'])
+    elif ARGS['--np-mmap']:
+        skrepresent(ARGS['<filename>'])
     elif ARGS['--np-memmap']:
         npmemmaprepresent(ARGS['<filename>'])
     elif ARGS['--np-fullcolor']:
